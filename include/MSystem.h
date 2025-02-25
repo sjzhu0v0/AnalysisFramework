fcf5ffe73d38c8fc2eda2b77a12ac8d8652e89d3
sjzhu0v0 <sjzhu0v0@gmail.com>
2025-02-25 16:10:43 +0100

    add multiThread

---
+++ include/MSystem.h
@@ -0,0 +1,84 @@
+#ifndef MSystem_h
+#define MSystem_h
+
+#include "fstream"
+#include "iostream"
+#include "nlohmann/json.hpp"
+#include "string"
+
+int GenerateUID() {
+  static int uid = 0;
+  return uid++;
+}
+
+using std::cout;
+using std::endl;
+using std::string;
+using json = nlohmann::json;
+
+json gJson;
+bool doVarExist(std::string nameNode1, std::string nameNode2,
+                json json4read = gJson) {
+  return json4read[nameNode1][nameNode2].is_null();
+}
+
+template <typename T>
+T GetJsonData(std::string nameNode1, std::string nameNode2,
+              json json4read = gJson) {
+  try {
+    T data = json4read[nameNode1][nameNode2].get<T>();
+    return data;
+  } catch (json::type_error &e) {
+    cout << "Error: " << e.what() << endl;
+    cout << "Error at json file: { " << nameNode1 << " : " << nameNode2 << " }"
+         << endl;
+    exit(1);
+  }
+  return T();
+}
+
+#define ConfigurableDefine(name)                                               \
+  template <typename T> struct Configurable##name {                            \
+    std::string fNameNode = #name;                                             \
+    T data;                                                                    \
+    Configurable##name(string d, T value_default) {                            \
+      data = value_default;                                                    \
+      if (!doVarExist(fNameNode, d))                                           \
+        data = GetJsonData<T>(fNameNode, d);                                   \
+    }                                                                          \
+  };
+
+#define funcWithJson(type, name)                                               \
+  ConfigurableDefine(name) template <typename T>                               \
+  using Configurable = Configurable##name<T>;                                  \
+  type name
+
+void SetUpJson(string config) {
+  // if json is empty, read from file
+  if (gJson.empty()) {
+    std::ifstream i(config);
+    i >> gJson;
+  }
+}
+
+#ifdef MultiThread
+
+#include <mutex>
+#include <thread>
+
+std::mutex mtx;
+
+struct gStrParameter4Thread;
+
+void MultiThread(int nThreads, std::function<void(int)> func) {
+  std::vector<std::thread> threads;
+  for (int i = 0; i < nThreads; i++) {
+    threads.push_back(std::thread(func, i));
+  }
+  for (int i = 0; i < nThreads; i++) {
+    threads[i].join();
+  }
+}
+
+#endif
+#endif
