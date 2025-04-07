441e7167ac11e69759e39ffb51f470df2cef8a15
sjzhu0v0 <sjzhu0v0@gmail.com>
2025-04-07 21:28:33 +0200

    add processing bar to ME_PR

--- macro/ME_PR.cpp
+++ macro/ME_PR.cpp
@@ -54,9 +54,9 @@ funcWithJson(void, ME_PR)(TString path_config = "../config.json") {
                            {-4., 4.});
   StrVar4Hist var_DeltaPhi("DeltaPhi", "#Delta #phi", "rad", 30,
                            {-M_PI_2, M_PI + M_PI_2});
-  StrVar4Hist var_VtxZ("fVtxZ", "V_{Z}", "cm", 20, {8, -10, 10});
-  StrVar4Hist var_Mass("fMass", "Mass", "GeV/c^{2}", 20, {100, 1., 5.});
-  StrVar4Hist var_Pt("fPT", "p_{T}", "GeV/c", 20, {10, 0., 10.});
+  StrVar4Hist var_VtxZ("fVtxZ", "V_{Z}", "cm", 8, {-10, 10});
+  StrVar4Hist var_Mass("fMass", "Mass", "GeV/c^{2}", 100, {1., 5.});
+  StrVar4Hist var_Pt("fPT", "p_{T}", "GeV/c", 10, {0., 10.});
   /* #endregion */
   vector<StrVar4Hist> vec_vars = {var_DeltaEta, var_DeltaPhi};
 
