21a79743803e3743ee168d4a8609feb63387e65a
sjzhu0v0 <sjzhu0v0@gmail.com>
2025-04-07 15:30:39 +0200

    debug

--- macro/plot_PR.cpp
+++ macro/plot_PR.cpp
@@ -11,44 +11,76 @@ funcWithJson(void, plot_PR)(TString path_config = "../config.json") {
   SetUpJson(path_config.Data());
   SetUpGStyle();
 
-  Configurable<string> config_pathSEInputFile(
-      "path_input_se",
+  Configurable<string> config_pathSE_PR_InputFile(
+      "path_input_SE_PR",
       "/home/szhu/work/alice/analysis/PairFlow/output/SE_PR.root");
-  Configurable<string> config_pathMEInputFile(
-      "path_input_me",
+  Configurable<string> config_pathME_PR_InputFile(
+      "path_input_ME_PR",
       "/home/szhu/work/alice/analysis/PairFlow/output/ME_PR.root");
+  Configurable<string> config_pathSE_RR_InputFile(
+      "path_input_SE_RR",
+      "/home/szhu/work/alice/analysis/PairFlow/output/SE_RR.root");
+  Configurable<string> config_pathME_RR_InputFile(
+      "path_input_ME_RR",
+      "/home/szhu/work/alice/analysis/PairFlow/output/ME_RR.root");
 
   // DeltaPhi_DeltaEta
-  TFile *file_input = TFile::Open(config_pathSEInputFile.data.c_str());
-  TH2D *h_DeltaPhi_DeltaEta_SE = (TH2D *)file_input->Get("DeltaEta_DeltaPhi");
-  TFile *file_input_me = TFile::Open(config_pathMEInputFile.data.c_str());
-  TH2D *h_DeltaPhi_DeltaEta_ME =
-      (TH2D *)file_input_me->Get("DeltaEta_DeltaPhi");
+  TFile *file_input_pr = TFile::Open(config_pathSE_PR_InputFile.data.c_str());
+  TH2D *h_DeltaPhi_DeltaEta_SE_PR =
+      (TH2D *)file_input_pr->Get("DeltaEta_DeltaPhi");
+  TFile *file_input_me_pr =
+      TFile::Open(config_pathME_PR_InputFile.data.c_str());
+  TH2D *h_DeltaPhi_DeltaEta_ME_PR =
+      (TH2D *)file_input_me_pr->Get("DeltaEta_DeltaPhi");
 
-  TH2D *h_se2me = (TH2D *)h_DeltaPhi_DeltaEta_SE->Clone("h_se2me");
+  TH2D *h_se2me = (TH2D *)h_DeltaPhi_DeltaEta_SE_PR->Clone("h_se2me");
   h_se2me->SetTitle("R(#Delta #eta, #Delta #phi)");
   h_se2me->SetZTitle("R(#Delta #eta, #Delta #phi)");
-  h_se2me->Divide(h_DeltaPhi_DeltaEta_ME);
+  h_se2me->Divide(h_DeltaPhi_DeltaEta_ME_PR);
 
   h_se2me->GetXaxis()->SetRangeUser(-2., 2.);
-  h_se2me->Scale(h_DeltaPhi_DeltaEta_ME->Integral() /
-                 h_DeltaPhi_DeltaEta_SE->Integral());
-  // TCanvas *c_DeltaPhi_DeltaEta =
-  //     new TCanvas("c_DeltaPhi_DeltaEta", "c_DeltaPhi_DeltaEta", 600, 600);
-  // StyleFlow::DeltaPhi_DeltaEta(c_DeltaPhi_DeltaEta, h_DeltaPhi_DeltaEta);
+  h_se2me->Scale(h_DeltaPhi_DeltaEta_ME_PR->Integral() /
+                 h_DeltaPhi_DeltaEta_SE_PR->Integral());
 
   TCanvas *c1 = new TCanvas("c1", "c1", 600, 600);
   c1->Divide(2, 2);
   // get the first pad
   TPad *pad1 = (TPad *)c1->cd(1);
-  StyleFlow::DeltaPhi_DeltaEta(pad1, h_DeltaPhi_DeltaEta_SE);
+  StyleFlow::DeltaPhi_DeltaEta(pad1, h_DeltaPhi_DeltaEta_SE_PR);
   TPad *pad2 = (TPad *)c1->cd(2);
-  StyleFlow::DeltaPhi_DeltaEta(pad2, h_DeltaPhi_DeltaEta_ME);
+  StyleFlow::DeltaPhi_DeltaEta(pad2, h_DeltaPhi_DeltaEta_ME_PR);
   TPad *pad3 = (TPad *)c1->cd(3);
   StyleFlow::DeltaPhi_DeltaEta(pad3, h_se2me);
 
   TCanvas *c2 = new TCanvas("c2", "c2", 600, 600);
   StyleFlow::DeltaPhi_DeltaEta(c2, h_se2me);
+
+
+  TFile *file_input_rr = TFile::Open(config_pathSE_RR_InputFile.data.c_str());
+  TH2D *h_DeltaPhi_DeltaEta_SE_RR =
+      (TH2D *)file_input_rr->Get("DeltaEta_DeltaPhi");
+  TFile *file_input_me_rr =
+      TFile::Open(config_pathME_RR_InputFile.data.c_str());
+  TH2D *h_DeltaPhi_DeltaEta_ME_RR =
+      (TH2D *)file_input_me_rr->Get("DeltaEta_DeltaPhi");
+  TH2D *h_se2me_rr = (TH2D *)h_DeltaPhi_DeltaEta_SE_RR->Clone("h_se2me_rr");
+  h_se2me_rr->SetTitle("R(#Delta #eta, #Delta #phi)");
+  h_se2me_rr->SetZTitle("R(#Delta #eta, #Delta #phi)");
+  h_se2me_rr->Divide(h_DeltaPhi_DeltaEta_ME_RR);
+  h_se2me_rr->GetXaxis()->SetRangeUser(-2., 2.);
+  h_se2me_rr->Scale(h_DeltaPhi_DeltaEta_ME_RR->Integral() /
+                    h_DeltaPhi_DeltaEta_SE_RR->Integral());
+  TCanvas *c3 = new TCanvas("c3", "c3", 600, 600);
+  c3->Divide(2, 2);
+  // get the first pad
+  TPad *pad4 = (TPad *)c3->cd(1);
+  StyleFlow::DeltaPhi_DeltaEta(pad4, h_DeltaPhi_DeltaEta_SE_RR);
+  TPad *pad5 = (TPad *)c3->cd(2);
+  StyleFlow::DeltaPhi_DeltaEta(pad5, h_DeltaPhi_DeltaEta_ME_RR);
+  TPad *pad6 = (TPad *)c3->cd(3);
+  StyleFlow::DeltaPhi_DeltaEta(pad6, h_se2me_rr);
+  TCanvas *c4 = new TCanvas("c4", "c4", 600, 600);
+  StyleFlow::DeltaPhi_DeltaEta(c4, h_se2me_rr);
 }
 
 int main(int argc, char **argv) {
