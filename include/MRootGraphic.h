547f3f3a8cc60fcaac0dc3aa55f270816aebf6af
sjzhu0v0 <sjzhu0v0@gmail.com>
2025-06-07 23:33:43 +0200

    Update

--- include/MRootGraphic.h
+++ include/MRootGraphic.h
@@ -124,6 +124,29 @@ void StyleCommon(TStyle *style = gStyle) {
   style->SetPadRightMargin(0.05);
 }
 
+template <typename T> void StyleHistCommon(T *hist_mb) {
+  hist_mb->GetXaxis()->SetLabelSize(0.03);
+  hist_mb->GetXaxis()->SetTitleSize(0.04);
+  hist_mb->GetXaxis()->SetTitleOffset(1.2);
+  hist_mb->GetYaxis()->SetLabelSize(0.03);
+  hist_mb->GetYaxis()->SetTitleSize(0.04);
+  hist_mb->GetYaxis()->SetTitleOffset(1.2);
+  hist_mb->GetXaxis()->SetNdivisions(505);
+  hist_mb->GetXaxis()->SetTickLength(0.02);
+  hist_mb->GetYaxis()->SetNdivisions(10);
+  hist_mb->GetYaxis()->SetTickLength(0.02);
+  hist_mb->GetXaxis()->SetLabelOffset(0.01);
+  hist_mb->GetYaxis()->SetLabelOffset(0.01);
+  hist_mb->GetXaxis()->SetLabelFont(42);
+  hist_mb->GetYaxis()->SetLabelFont(42);
+  hist_mb->GetXaxis()->SetTitleFont(42);
+  hist_mb->GetYaxis()->SetTitleFont(42);
+  hist_mb->GetXaxis()->SetTickLength(0.02);
+  hist_mb->GetYaxis()->SetTickLength(0.02);
+  hist_mb->GetXaxis()->CenterTitle();
+  hist_mb->GetYaxis()->CenterTitle();
+}
+
 void StyleHistCommonHist(TH1 *hist_mb) {
   hist_mb->GetXaxis()->SetLabelSize(0.03);
   hist_mb->GetXaxis()->SetTitleSize(0.04);
@@ -147,29 +170,52 @@ void StyleHistCommonHist(TH1 *hist_mb) {
   hist_mb->GetYaxis()->CenterTitle();
 }
 
-void StyleHistCommonGraph(TGraph *hist_mb) {
-  hist_mb->GetXaxis()->SetLabelSize(0.03);
-  hist_mb->GetXaxis()->SetTitleSize(0.04);
-  hist_mb->GetXaxis()->SetTitleOffset(1.2);
-  hist_mb->GetYaxis()->SetLabelSize(0.03);
-  hist_mb->GetYaxis()->SetTitleSize(0.04);
-  hist_mb->GetYaxis()->SetTitleOffset(1.2);
-  hist_mb->GetXaxis()->SetNdivisions(505);
-  hist_mb->GetXaxis()->SetTickLength(0.02);
-  hist_mb->GetYaxis()->SetNdivisions(10);
-  hist_mb->GetYaxis()->SetTickLength(0.02);
-  hist_mb->GetXaxis()->SetLabelOffset(0.01);
-  hist_mb->GetYaxis()->SetLabelOffset(0.01);
-  hist_mb->GetXaxis()->SetLabelFont(42);
-  hist_mb->GetYaxis()->SetLabelFont(42);
-  hist_mb->GetXaxis()->SetTitleFont(42);
-  hist_mb->GetYaxis()->SetTitleFont(42);
-  hist_mb->GetXaxis()->SetTickLength(0.02);
-  hist_mb->GetYaxis()->SetTickLength(0.02);
-  hist_mb->GetXaxis()->CenterTitle();
-  hist_mb->GetYaxis()->CenterTitle();
-  hist_mb->SetLineWidth(2);
-}
+// void StyleHistCommonProfile(TProfile *hist_mb) {
+//   hist_mb->GetXaxis()->SetLabelSize(0.03);
+//   hist_mb->GetXaxis()->SetTitleSize(0.04);
+//   hist_mb->GetXaxis()->SetTitleOffset(1.2);
+//   hist_mb->GetYaxis()->SetLabelSize(0.03);
+//   hist_mb->GetYaxis()->SetTitleSize(0.04);
+//   hist_mb->GetYaxis()->SetTitleOffset(1.2);
+//   hist_mb->GetXaxis()->SetNdivisions(505);
+//   hist_mb->GetXaxis()->SetTickLength(0.02);
+//   hist_mb->GetYaxis()->SetNdivisions(10);
+//   hist_mb->GetYaxis()->SetTickLength(0.02);
+//   hist_mb->GetXaxis()->SetLabelOffset(0.01);
+//   hist_mb->GetYaxis()->SetLabelOffset(0.01);
+//   hist_mb->GetXaxis()->SetLabelFont(42);
+//   hist_mb->GetYaxis()->SetLabelFont(42);
+//   hist_mb->GetXaxis()->SetTitleFont(42);
+//   hist_mb->GetYaxis()->SetTitleFont(42);
+//   hist_mb->GetXaxis()->SetTickLength(0.02);
+//   hist_mb->GetYaxis()->SetTickLength(0.02);
+//   hist_mb->GetXaxis()->CenterTitle();
+//   hist_mb->GetYaxis()->CenterTitle();
+// }
+
+// void StyleHistCommonGraph(TGraph *hist_mb) {
+//   hist_mb->GetXaxis()->SetLabelSize(0.03);
+//   hist_mb->GetXaxis()->SetTitleSize(0.04);
+//   hist_mb->GetXaxis()->SetTitleOffset(1.2);
+//   hist_mb->GetYaxis()->SetLabelSize(0.03);
+//   hist_mb->GetYaxis()->SetTitleSize(0.04);
+//   hist_mb->GetYaxis()->SetTitleOffset(1.2);
+//   hist_mb->GetXaxis()->SetNdivisions(505);
+//   hist_mb->GetXaxis()->SetTickLength(0.02);
+//   hist_mb->GetYaxis()->SetNdivisions(10);
+//   hist_mb->GetYaxis()->SetTickLength(0.02);
+//   hist_mb->GetXaxis()->SetLabelOffset(0.01);
+//   hist_mb->GetYaxis()->SetLabelOffset(0.01);
+//   hist_mb->GetXaxis()->SetLabelFont(42);
+//   hist_mb->GetYaxis()->SetLabelFont(42);
+//   hist_mb->GetXaxis()->SetTitleFont(42);
+//   hist_mb->GetYaxis()->SetTitleFont(42);
+//   hist_mb->GetXaxis()->SetTickLength(0.02);
+//   hist_mb->GetYaxis()->SetTickLength(0.02);
+//   hist_mb->GetXaxis()->CenterTitle();
+//   hist_mb->GetYaxis()->CenterTitle();
+//   hist_mb->SetLineWidth(2);
+// }
 } // namespace MRootGraphic
 
 class MPublisherCanvas {
